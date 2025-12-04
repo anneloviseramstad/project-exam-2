b8e0a813404784c9c43a5ab141f75a2f50056560
Anne Ramstad <annram02249@stud.noroff.no>
2025-12-04 20:06:46 +0100

    Bug FIx

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="relative min-h-[40vh] md:h-[60vh] bg-cover bg-center shadow flex items-center"
+    class="relative min-h-[50vh] md:h-[60vh] bg-cover bg-center shadow flex items-center"
     style="
       background-image: url('https://images.unsplash.com/photo-1554205163-beedc3a3e9fd?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
     "
